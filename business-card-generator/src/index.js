fbaffd99ec141b785484103fb60405ed7d2ba61e
Erick Wendel <erick.workspace@gmail.com>
2020-09-07 18:58:37 -0300

    cp approach
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- business-card-generator/src/index.js
+++ business-card-generator/src/index.js
@@ -1,14 +1,15 @@
-import data from '../resources/data.json'
-import { Worker } from 'worker_threads'
-import { dirname } from 'path'
-const { pathname: currentFile } = new URL(import.meta.url)
-const cwd = dirname(currentFile)
+const data = require('../resources/data.json')
+const cp = require('child_process')
+const cwd = __dirname
 const moduleName = `${cwd}/worker.js`
 
-for (const item of [data[0]]) {
-    const worker = new Worker(moduleName, { workerData: item })
+for (const item of data) {
+    console.log('data', item.name)
+    const worker = cp.fork(moduleName, [])
+    
     worker.on('message', msg => console.log(msg))
     worker.on('error', msg => console.error('error!', msg))
-    // worker.postMessage(item)
+ 
+    worker.send(item);
 }
 
--- business-card-generator/src/worker.js
+++ business-card-generator/src/worker.js
@@ -1,24 +1,17 @@
-import { parentPort, threadId, workerData } from 'worker_threads'
-// import Nightmare from 'nightmare';
-import querystring from 'querystring'
-import { v1 } from 'uuid'
-import { dirname } from 'path'
-import puppeteer from 'puppeteer';
+const querystring = require('querystring')
+const { v1 } = require('uuid')
+const { join } = require('path')
+const puppeteer = require('puppeteer')
 
-const { pathname: currentFile } = new URL(import.meta.url)
-const cwd = dirname(currentFile)
+const cwd = __dirname
 
 const BC_URL = 'https://erickwendel.github.io/business-card-template/index.html'
 
 
-// const nightmare = Nightmare({
-//     // show: true
-// });
-
 async function render({ finalUrl, name }) {
     const WIDTH = 1920;
     const HEIGHT = 1080;
-    const output = `${cwd}/../output/${name}-${v1()}.pdf`
+    const output = join(cwd, `/../output/${name}-${v1()}.pdf`)
 
     const browser = await puppeteer.launch({
         // headless: false,
@@ -50,22 +43,22 @@ function createQueryStringFromObject(data) {
 }
 
 async function main(data) {
+    const threadId = process.pid
     console.log(`${threadId} got message: ${data.name}`);
     const qs = createQueryStringFromObject(data);
     const finalUrl = `${BC_URL}?${qs}`
 
     try {
         await render({ finalUrl, name: data.name })
-        parentPort.postMessage(`${threadId} has finished`)
+        process.send(`${threadId} has finished`)
     } catch (error) {
-        parentPort.postMessage(`${threadId} has crashed: ${error.stack}`)
+        process.send(`${threadId} has crashed: ${error.stack}`)
     }
 
 }
 
+process.once('message', main)
 
-// parentPort.once('message', main);
-main(workerData)
 
 
 
